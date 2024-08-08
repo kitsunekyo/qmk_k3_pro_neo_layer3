15e2006b0257051f6e3d5e630ee67029532809fc
Alex Spieslechner <alexander.spieslechner@gmail.com>
2024-08-08 14:49:52 +0200

    feat: reduce tapping timeout to 100

--- config.h
+++ config.h
@@ -1,2 +1,7 @@
+/**
+ * custom behavior configuration
+ * @see {@link https://docs.qmk.fm/config_options#behaviors-that-can-be-configured}
+ */
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define TAPPING_TERM 100
