a4a547edec41bd76025e5581e9ca4d48aa7ca3e0
Alex Spieslechner <alexander.spieslechner@dynatrace.com>
2025-05-29 09:25:10 +0200

    feat: finally implement CHORDAL_HOLD

--- rules.mk
+++ rules.mk
@@ -1,3 +1,7 @@
 CAPS_WORD_ENABLE = yes
-# save some space
-EXTRAFLAGS += -flto
+# https://docs.qmk.fm/squeezing_avr#rules-mk-settings
+LTO_ENABLE = yes
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+MOUSEKEY_ENABLE = no
+SPACE_CADET_ENABLE = no
